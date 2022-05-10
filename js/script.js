a74c5b401f102e1115fea6d198a0b9851c3c9f7a
jykyle0226 <jykyle0226@gmail.com>
2022-05-10 15:32:23 -0400

    different API

--- js/script.js
+++ js/script.js
@@ -1,19 +1,31 @@
-const accessToken = "e14529b1805945dd9fa8ea9fb64cb3be"
+const apiKey = "TY3ijVkOquSjEXdbNNoaPtHZ5V7RnzIc"
+const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=1&q=`
+const search = document.getElementById('search').value.trim()
+const fig = document.createElement("figure");
+const img = document.createElement("img");
+const fc = document.createElement("figcaption");
 
-$.ajax({
-    method: "GET",
-    url: " https://api.spotify.com",
-    headers: {
-      Authorization: `Bearer ${accessToken}`
-    },
-    data: {
-      "name": "She's So Unusual",
-      "type": "album",
-      "external_urls" : {
-      "spotify" : "https://open.spotify.com/album/0sNOF9WDwhWunNAHPD3Baj"
-      }
-    }
+document.addEventListener("DOMContentLoaded", init)
+
+function init() {
+  document.getElementById("btnSearch").addEventListener("click", function(evt){
+    evt.preventDefault()
+    url = url.concat(search)
+    console.log(url)
+    fetch(url)
+      .then(response => response.json)
+      .then(content => {
+        console.log(content.data)
+        console.log("META", content.data)
+        img.src = content.data[0].images.downsized.url;
+        img.alt = content.data[0].title;
+        fc.textContent = content.data[0].title;
+        fig.appendChild(img);
+        fig.appendChild(fc);
+        let out = document.querySelector(".out");
+        out.insertAdjacentElement("afterbegin", fig);
+        document.querySelector("#search").value = "";
+      })
   })
-    .done(function( msg ) {
-      console.log(msg)
-    })
+
+}
