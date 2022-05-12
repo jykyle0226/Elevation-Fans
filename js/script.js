5de3c34931fb41a67137cedfc63156acd71a64a0
jykyle0226 <jykyle0226@gmail.com>
2022-05-12 00:42:52 -0400

    now functional. need polishing

--- js/script.js
+++ js/script.js
@@ -1,20 +1,31 @@
+// function getData(){
+
+//   var input = $("#searchtext").val()
+//   var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=TY3ijVkOquSjEXdbNNoaPtHZ5V7RnzIc&limit=30")
+//   console.log(input)
+// xhr.done(function(data) {
+//   console.log(data)
+//   var gifs = data.data
+//   var img = gifs[0].images.original.url
+//   console.log(img)
+// for(i in gifs) 
+// {
+//   $('.out').append(`<img src=${img}/>`)
+// }
+// })
+
+// }
+
+
 function getData(){
-
   var input = $("#searchtext").val()
-  var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=TY3ijVkOquSjEXdbNNoaPtHZ5V7RnzIc&limit=30")
-
-xhr.done(function(data) {
-  console.log("success got data", data)
-  var gifs = data.data
-  var img = gifs[0].images.original.url
-  console.log(img)
-for(i in gifs) 
-{
-  $('.out').append(`<img src=${img}/>`)
+  var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+input+"+&api_key=TY3ijVkOquSjEXdbNNoaPtHZ5V7RnzIc&limit=30")
+  xhr.done(function(data){
+    console.log(data)
+    var gifs = data.data
+    for (i in gifs) 
+    {
+      $('.out').append("<img src='"+gifs[i].images.original.url+"' style='height:350px; width:350px;'/>")
+    }
+  })
 }
-})
-
-}
-
-
-
