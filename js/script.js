38633e7b842e9a9fb7d02df9328766d269fae0ab
jykyle0226 <jykyle0226@gmail.com>
2022-05-13 10:31:31 -0400

    JS edit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-function getData(){
-  const deletbtn = document.getElementById('delete')
+function getData(event){
+  event.preventDefault()
   const APIkey = "TY3ijVkOquSjEXdbNNoaPtHZ5V7RnzIc"
-  const input = $("#searchtext").val()
   const url = $.ajax(`https://api.giphy.com/v1/gifs/search?q="${input}"+&api_key=${APIkey}&limit=28`)
   const newGif = document.querySelector('.out')
+  const input = $("#searchtext").val()
+  const deletbtn = document.getElementById('delete')
 
 
   url.done(function(data){
-
     console.log(data)
     const gifs = data.data
 
@@ -15,9 +15,11 @@ function getData(){
     {
       $('.out').append("<img src='"+gifs[i].images.original.url+"' style='height:350px; width:350px;'/>")
     }
+
     function deleteGif() {
       newGif.parentNode.removeChild(newGif)
     }
+
     deletbtn.onclick = deleteGif
   })
 }
