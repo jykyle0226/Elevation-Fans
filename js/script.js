4338f2741df147e76d6f6419b8f9c18d5ee20b8b
jykyle0226 <jykyle0226@gmail.com>
2022-05-13 10:32:18 -0400

    preventDefault

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function getData(event){
   event.preventDefault()
   const APIkey = "TY3ijVkOquSjEXdbNNoaPtHZ5V7RnzIc"
-  const url = $.ajax(`https://api.giphy.com/v1/gifs/search?q="${input}"+&api_key=${APIkey}&limit=28`)
+  const url = $.ajax(`https://api.giphy.com/v1/gifs/search?q="${input}"+&api_key=${APIkey}&limit=4`)
   const newGif = document.querySelector('.out')
   const input = $("#searchtext").val()
   const deletbtn = document.getElementById('delete')
